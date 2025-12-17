e066740b665e67d5f750cf796a03326307798203
Johnny <esp25goodlife@gmail.com>
2025-12-17 12:38:16 +0200

    one task left

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const addMessage = function () {
   const userMessage = document.getElementById('feedback');
   const boxMessages = document.getElementById('wrapMessages');
   console.log(userMessage);
-  boxMessages.innerHTML += `<div class="d-flex justify-content-between">
+  boxMessages.innerHTML += `<div class="d-flex justify-content-between mt-5">
           <p>${userName.value}</p>
           <p>${new Date()}</p>
         </div>
-        <div>${userMessage.value}</div>`;
+        <div class="mb-5">${userMessage.value}</div>`;
 };
+
+const registerForm = document.getElementById('submitForm');
+registerForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const password = e.target.password.value;
+  const checkPassword = e.target.checkPassword.value;
+  if (password === checkPassword) {
+    alert('the data was send');
+    registerForm.reset();
+  } else {
+    alert(' passwords is different');
+  }
+});
+
+// ---------------------------task 3 -------------------
+const newColor = document.getElementById('form__colors');
+newColor = addEventListener('submit', e => {
+  e.defaultPrevented();
+  const r = e.target.red.value;
+  const g = e.target.green.value;
+  const b = e.target.blue.value;
+});
