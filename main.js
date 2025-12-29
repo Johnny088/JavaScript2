291e79b68e7eee863d71e8732a10ba9df67742f1
Johnny <esp25goodlife@gmail.com>
2025-12-29 13:27:56 +0200

    added the second task

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 const time = document.getElementById('time');
-const currentTime = new Date();
-time.addEventListener('change', () => {
-  time.textContent = currentTime;
-});
+setInterval(() => {
+  const currentTime = new Date();
+  time.value = currentTime.toLocaleTimeString();
+}, 1000);
+
+// ------------------ task 2 ------------------------
+const setTimer = function () {
+  const minutes = document.getElementById('minutes');
+  const seconds = document.getElementById('seconds');
+  const timeout = document.getElementById('timeout');
+  let m = Number(minutes.value);
+  let s = Number(seconds.value);
+
+  const interval = setInterval(() => {
+    s -= 1;
+    seconds.value = s;
+    if (m == 0 && s == 0) {
+      minutes.value = 0;
+      seconds.value = 0;
+      clearInterval(interval);
+      timeout.textContent = 'Time is over';
+      console.log('Time is over');
+    } else if (s == -1) {
+      m -= 1;
+      minutes.value = m;
+      s = 59;
+      seconds.value = s;
+    }
+  }, 1000);
+};
