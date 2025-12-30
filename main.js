ec406f543c076fc7961135d819a16c5e82d9811a
Johnny <esp25goodlife@gmail.com>
2025-12-30 13:04:27 +0200

    done

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const setTimer = function () {
   const minutes = document.getElementById('minutes');
   const seconds = document.getElementById('seconds');
   const timeout = document.getElementById('timeout');
+  const stopBtn = document.getElementById('stopBtn');
+
   let m = Number(minutes.value);
   let s = Number(seconds.value);
 
@@ -20,7 +22,6 @@ const setTimer = function () {
       seconds.value = 0;
       clearInterval(interval);
       timeout.textContent = 'Time is over';
-      console.log('Time is over');
     } else if (s == -1) {
       m -= 1;
       minutes.value = m;
@@ -28,4 +29,51 @@ const setTimer = function () {
       seconds.value = s;
     }
   }, 1000);
+  stopBtn.addEventListener('click', () => clearInterval(interval));
+};
+
+// ------------------------------------------- task3 ------------------------------------------------------
+const startStopwatch = function () {
+  const minutes = document.getElementById('min');
+  const seconds = document.getElementById('sec');
+  const msec = document.getElementById('ms');
+  const stopBtn = document.getElementById('stopBtnWatch');
+  const record = document.getElementById('record');
+  const clearStopwatch = document.getElementById('clearBtnWatch');
+  let flag = true;
+  let m = Number(minutes.value);
+  let s = Number(seconds.value);
+  let ms = Number(seconds.value);
+  const interval = setInterval(() => {
+    ms += 1;
+    msec.value = ms;
+    if (ms === 100) {
+      ms = 0;
+      msec.value = ms;
+      s += 1;
+      seconds.value = s;
+    }
+    if (s === 60) {
+      s = 0;
+      seconds.value = s;
+      m += 1;
+      minutes.value = m;
+    }
+  }, 10);
+  stopBtn.addEventListener('click', () => {
+    clearInterval(interval);
+    if (flag === false) {
+      record.innerHTML = '';
+      flag = true;
+    }
+    record.innerHTML += `<li>Result <input class="tImput ms-2 mt-3" value="${m}"/> <input class="tImput" value="${s}"/> <input class="tImput" value="${ms}"/></li>`;
+  });
+  clearStopwatch.addEventListener('click', () => {
+    clearInterval(interval);
+    record.innerHTML = '';
+    msec.value = ms = 0;
+    seconds.value = s = 0;
+    minutes.value = m = 0;
+    flag = false;
+  });
 };
