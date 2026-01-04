5a213347a29942aa9bd73c3f21a1331d0d50a197
Johnny <esp25goodlife@gmail.com>
2026-01-04 19:19:19 +0200

    added 3 hours interval of forecast

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const key = '58e68e25012fe2fb7cedfd77f7ff1196';
 // ----------------------------------------------------------------getting the geolocation -----------------------
 document.addEventListener('DOMContentLoaded', () => {
   navigator.geolocation.getCurrentPosition(successPosition, errorPosition);
@@ -34,10 +35,23 @@ const setUserCity = function (latitude, longitude) {
 };
 
 // -------------------------------------------- Forecats -----------------------------------------
-const city = 'Kyiv';
-const key = '58e68e25012fe2fb7cedfd77f7ff1196';
-const getOneDay = async function () {
-  try {
-    const result = await fetch(url);
-  } catch {}
+const getOneDay = async function (lat, lon) {
+  const hourlyURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+
+  fetch(hourlyURL)
+    .then(response => response.json())
+    .then(data => {
+      const hourly = data.list.slice(0, 6);
+      hourly.forEach(item => {
+        const time = item.dt_txt.split(' ')[1].slice(0, 5);
+        const temp = Math.round(item.main.temp - 273);
+        const feel = Math.round(item.main.feels_like - 273);
+        const state = item.weather[0].main;
+        const icon = item.weather[0].icon;
+        const wind = item.wind.speed;
+        console.log(time, temp, feel, state, icon, wind);
+      });
+    })
+    .catch(() => console.log('smth went wrong'));
 };
+getOneDay(48.4680221, 35.0417711);
